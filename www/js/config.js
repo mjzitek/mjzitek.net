7a186331201a1fc453838435862a04df02c5f84f
Mike Zitek <mzitek@gmail.com>
2015-03-08 20:24:23 -0700

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var config = {
-  ghost_reader_url : "http://localhost:9002/",
-  flickr_feed_url : "http://api.flickr.com/services/feeds/photos_public.gne?id=44099010@N00&lang=en-us&format=json&jsoncallback=?",
+  ghost_reader_url : "",
+  flickr_feed_url : "",
   flickr_photo_count : 4
 
 };
